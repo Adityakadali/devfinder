88cdc51aba075d1a084b78c1ef1b8cdb449b1d4f
AdityaKadali <adityakadali00@gmail.com>
2023-01-01 23:23:14 +0530

    Added search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid min-h-screen w-screen place-content-center">
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">devfinder</h1>
+        <span className="flex items-center gap-2">
+          LIGHT
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-6 w-6"
+          >
+            <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+          </svg>
+        </span>
+      </div>
+      <Searchbar />
     </div>
   );
 }
---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,37 @@
+const Searchbar = () => {
+  return (
+    <div className="mt-8 rounded-lg bg-slate-800 p-2 shadow-lg">
+      <form className="flex w-[42rem] items-center justify-between">
+        <div className="flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="ml-4 h-6 w-6 text-blue-600"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <input
+            className="w-96 bg-slate-800 px-3 py-2 text-xl focus:outline-none"
+            type="search"
+            name="serach"
+            id=""
+            placeholder="Search Github username..."
+          />
+        </div>
+        <button
+          type="submit"
+          className="rounded-lg bg-blue-500 px-4 py-2 text-xl font-semibold shadow-sm"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Searchbar;
